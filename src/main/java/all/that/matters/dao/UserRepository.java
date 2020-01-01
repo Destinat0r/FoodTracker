c9742b4c5964751a8e12119e294723446b3a4d7b
Destinat0r <ashavrn@protonmail.com>
2020-01-01 11:37:39 +0200

    added UserRepository and findByLogin()

---
+++ src/main/java/all/that/matters/dao/UserRepository.java
@@ -0,0 +1,13 @@
+package all.that.matters.dao;
+
+import all.that.matters.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByLogin(String login);
+}
