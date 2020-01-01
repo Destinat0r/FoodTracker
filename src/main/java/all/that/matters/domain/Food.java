3dc281dc38c71c8774f8ff10af3f49cc48b951bf
Destinat0r <ashavrn@protonmail.com>
2020-01-01 11:22:17 +0200

    added jpa annotations for User, created Food

---
+++ src/main/java/all/that/matters/domain/Food.java
@@ -0,0 +1,11 @@
+package all.that.matters.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Food {
+
+    private Long id;
+    private String name;
+    private double calories;
+}
--- src/main/java/all/that/matters/domain/User.java
+++ src/main/java/all/that/matters/domain/User.java
@@ -3,9 +3,7 @@ package all.that.matters.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @NoArgsConstructor
 @Data
@@ -15,8 +13,17 @@ import javax.persistence.Table;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "login", nullable = false, unique = true)
     private String login;
+
+    @Column(name = "password", nullable = false)
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
     private Role role;
 }
