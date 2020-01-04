ed960affed9f47f239bf3da79dcd1ec4f7de41a1
Destinat0r <ashavrn@protonmail.com>
2020-01-04 19:12:35 +0200

    added webSecurityConfig

--- src/main/java/all/that/matters/config/WebSecurityConfig.java
+++ src/main/java/all/that/matters/config/WebSecurityConfig.java
@@ -1,5 +1,25 @@
 package all.that.matters.config;
 
-public class WebSecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/", "/static/**", "/webjars/**").permitAll()
+                    .anyRequest().permitAll();
+    }
+
+    @Override protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    }
 }
