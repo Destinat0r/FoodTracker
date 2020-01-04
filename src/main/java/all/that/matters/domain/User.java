2d7fb76e21c93dcc8e565c34b6faa781117dc9ea
Destinat0r <ashavrn@protonmail.com>
2020-01-04 14:31:33 +0200

    implements UserDetails, implemented methods, added active variable

--- src/main/java/all/that/matters/domain/User.java
+++ src/main/java/all/that/matters/domain/User.java
@@ -3,8 +3,11 @@ package all.that.matters.domain;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.Set;
 
 @NoArgsConstructor
@@ -13,7 +16,7 @@ import java.util.Set;
 
 @Entity
 @Table(name = "users", uniqueConstraints={@UniqueConstraint(columnNames={"email"})})
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -32,9 +35,36 @@ public class User {
     @Column(name = "national_name", nullable = false)
     private String nationalName;
 
+    @Column(name = "active", nullable = false)
+    private boolean active;
+
     @Column(name = "role", nullable = false)
     @Enumerated(EnumType.STRING)
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     private Set<Role> roles;
+
+    @Override public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override public String getUsername() {
+        return null;
+    }
+
+    @Override public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override public boolean isEnabled() {
+        return false;
+    }
 }
