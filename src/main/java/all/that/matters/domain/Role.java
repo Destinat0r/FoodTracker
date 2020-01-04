05b0a07beed8664c28423dd77ec363a7d5b82f32
Destinat0r <ashavrn@protonmail.com>
2020-01-04 14:03:28 +0200

    added implements GrantedAuthority, getAuthority()

--- src/main/java/all/that/matters/domain/Role.java
+++ src/main/java/all/that/matters/domain/Role.java
@@ -1,5 +1,11 @@
 package all.that.matters.domain;
 
-public enum Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
     USER, ADMIN;
+
+    @Override public String getAuthority() {
+        return name();
+    }
 }
