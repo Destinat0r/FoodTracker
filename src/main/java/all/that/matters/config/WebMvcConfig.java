2637218e4fc5e7970c121b8bdcaf71143f870131
Destinat0r <ashavrn@protonmail.com>
2020-01-04 19:12:00 +0200

    added webmvc config

--- src/main/java/all/that/matters/config/WebMvcConfig.java
+++ src/main/java/all/that/matters/config/WebMvcConfig.java
@@ -1,5 +1,27 @@
 package all.that.matters.config;
 
-public class WebMvcConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.WebJarsResourceResolver;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebJarsResourceResolver webJarsResourceResolver() {
+        return new WebJarsResourceResolver();
+    }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("/webjars/");
+    }
 
 }
