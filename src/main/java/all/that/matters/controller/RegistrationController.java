64d47e4ef669609a968cd8e5f6d206dd17cec04c
Destinat0r <ashavrn@protonmail.com>
2020-01-04 21:01:32 +0200

    added registraton postmapping addUser() and form fields validations

--- src/main/java/all/that/matters/controller/RegistrationController.java
+++ src/main/java/all/that/matters/controller/RegistrationController.java
@@ -1,8 +1,17 @@
 package all.that.matters.controller;
 
+import all.that.matters.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 public class RegistrationController {
@@ -10,5 +19,32 @@ public class RegistrationController {
     @GetMapping("/registration")
     public String registration(@ModelAttribute("message") String message) {
         return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(
+            @RequestParam("passwordConfirm") String passwordConfirm,
+            @Valid User user,
+            BindingResult bindingResult,
+            Model model) {
+
+        boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirm);
+        if (isConfirmEmpty) {
+            model.addAttribute("passwordConfirmError", "password confirmation cannot be empty");
+        }
+
+        if (user.getPassword() != null && !user.getPassword().equals(passwordConfirm)) {
+            model.addAttribute("passwordError", "Passwords don't match");
+        }
+
+        if (isConfirmEmpty || bindingResult.hasErrors()) {
+            Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
+            model.mergeAttributes(errors);
+
+            return "registration";
+        }
+
+
+        return "redirect:/login";
     }
 }
