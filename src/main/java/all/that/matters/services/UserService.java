ebbbfa8be3d61d71209a2a1547d378b43d4b4660
Destinat0r <ashavrn@protonmail.com>
2020-01-05 14:58:18 +0200

    added findAll()

--- src/main/java/all/that/matters/services/UserService.java
+++ src/main/java/all/that/matters/services/UserService.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -32,5 +33,9 @@ public class UserService implements UserDetailsService {
         user.setRoles(Collections.singleton(Role.USER));
         user.setActive(true);
         userRepo.save(user);
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
     }
 }
