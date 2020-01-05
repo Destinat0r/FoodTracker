e621d90c99d9ba6fb1d28f2991a15f80f9fd6b4d
Destinat0r <ashavrn@protonmail.com>
2020-01-05 19:08:13 +0200

    added admin only @PreAuthorize

--- src/main/java/all/that/matters/services/FoodService.java
+++ src/main/java/all/that/matters/services/FoodService.java
@@ -3,6 +3,7 @@ package all.that.matters.services;
 import all.that.matters.dao.FoodRepository;
 import all.that.matters.domain.Food;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public class FoodService {
         this.foodRepository = foodRepository;
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     public List<Food> findAll() {
         return foodRepository.findAll();
     }
