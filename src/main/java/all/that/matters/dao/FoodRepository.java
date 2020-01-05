7e8043823a85cbc10f261aceec66476f2da34827
Destinat0r <ashavrn@protonmail.com>
2020-01-05 18:54:30 +0200

    added @Repository annotation

--- src/main/java/all/that/matters/dao/FoodRepository.java
+++ src/main/java/all/that/matters/dao/FoodRepository.java
@@ -2,7 +2,9 @@ package all.that.matters.dao;
 
 import all.that.matters.domain.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FoodRepository extends JpaRepository<Food, Long> {
-    
+
 }
