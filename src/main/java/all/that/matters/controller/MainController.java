665d99479106154f0bf65253d6de30b75f3c0549
Destinat0r <ashavrn@protonmail.com>
2020-01-06 00:00:08 +0200

    added post mapping for main, get for login

--- src/main/java/all/that/matters/controller/MainController.java
+++ src/main/java/all/that/matters/controller/MainController.java
@@ -4,6 +4,9 @@ import all.that.matters.domain.Role;
 import all.that.matters.domain.User;
 import all.that.matters.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,18 +29,26 @@ public class MainController {
         return "index";
     }
 
-    @PostMapping("/login")
-    public String login(
-            @ModelAttribute("user") User user) {
+    @GetMapping("/main")
+    public String login() {
 
-        User userFromDb = (User) userService.loadUserByUsername(user.getUsername());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 
-        if (userFromDb.getRoles().contains(Role.ADMIN)) {
+        User user = (User) userDetails;
+
+        if (user.getRoles().contains(Role.ADMIN)) {
             return "admin_main";
         }
 
         return "main";
     }
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "login";
+    }
+
 
     @GetMapping("/users")
     public String getUsers(Model model) {
