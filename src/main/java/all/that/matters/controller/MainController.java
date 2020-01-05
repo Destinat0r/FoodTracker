ca70df00db84585872529e6258f293464d6beb1d
Destinat0r <ashavrn@protonmail.com>
2020-01-05 17:36:22 +0200

    added user service, get for index, post for login and get for users

--- src/main/java/all/that/matters/controller/MainController.java
+++ src/main/java/all/that/matters/controller/MainController.java
@@ -2,24 +2,46 @@ package all.that.matters.controller;
 
 import all.that.matters.domain.Role;
 import all.that.matters.domain.User;
+import all.that.matters.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
 
+    private UserService userService;
+
+    @Autowired
+    public MainController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("user", new User());
         return "index";
     }
 
-    @GetMapping("/login")
-    public String login(User user) {
-        if (user.getRoles().contains(Role.ADMIN)) {
-            return "users";
+    @PostMapping("/login")
+    public String login(
+            @ModelAttribute("user") User user) {
+
+        User userFromDb = (User) userService.loadUserByUsername(user.getUsername());
+
+        if (userFromDb.getRoles().contains(Role.ADMIN)) {
+            return "redirect:/users";
         }
 
         return "main";
+    }
+
+    @GetMapping("/users")
+    public String getUsers(Model model) {
+        model.addAttribute("users", userService.findAll());
+        return "users";
     }
 }
