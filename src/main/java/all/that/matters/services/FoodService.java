c334bd6d454d0fd9f40c1265273b89d3ed6b98d4
Destinat0r <ashavrn@protonmail.com>
2020-01-05 18:54:47 +0200

    added foodService

--- src/main/java/all/that/matters/services/FoodService.java
+++ src/main/java/all/that/matters/services/FoodService.java
@@ -1,5 +1,23 @@
 package all.that.matters.services;
 
+import all.that.matters.dao.FoodRepository;
+import all.that.matters.domain.Food;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class FoodService {
 
+    private FoodRepository foodRepository;
+
+    @Autowired
+    public FoodService(FoodRepository foodRepository) {
+        this.foodRepository = foodRepository;
+    }
+
+    public List<Food> findAll() {
+        return foodRepository.findAll();
+    }
 }
