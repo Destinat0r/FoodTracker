3d65c74d66850179aabb007bd7aba71aa62d8be4
Destinat0r <ashavrn@protonmail.com>
2020-01-05 21:22:11 +0200

    added viewController for login and resource handler for static

--- src/main/java/all/that/matters/config/WebMvcConfig.java
+++ src/main/java/all/that/matters/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package all.that.matters.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.WebJarsResourceResolver;
 
@@ -14,9 +15,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
         return new WebJarsResourceResolver();
     }
 
+    @Override public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("/webjars/");
+
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
