c4292d32f75965e537fe1c94decfbc8f53856a9d
Destinat0r <ashavrn@protonmail.com>
2020-01-05 09:00:25 +0200

    added /registration to permitted all requests

--- src/main/java/all/that/matters/config/WebSecurityConfig.java
+++ src/main/java/all/that/matters/config/WebSecurityConfig.java
@@ -15,7 +15,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/static/**", "/webjars/**").permitAll()
+                    .antMatchers("/", "/static/**", "/webjars/**", "/registration").permitAll()
                     .anyRequest().permitAll();
     }
 
