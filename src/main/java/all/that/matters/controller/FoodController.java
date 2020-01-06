a4371f317b9754de9aeecd5719e800b741506d7d
Destinat0r <ashavrn@protonmail.com>
2020-01-06 16:04:00 +0200

    added addToAll()

--- src/main/java/all/that/matters/controller/FoodController.java
+++ src/main/java/all/that/matters/controller/FoodController.java
@@ -6,10 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/food")
@@ -30,5 +35,21 @@ public class FoodController {
         model.addAttribute("allFood", allFood);
 
         return "all_food";
+    }
+
+    @PostMapping("/add_to_all")
+    public String addToAll(
+            @Valid Food food,
+            BindingResult bindingResult,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
+            model.mergeAttributes(errors);
+        }
+
+        foodService.add(food);
+
+        return "redirect:/food/all";
     }
 }
