50efe937061db9170e1206efc05dca2f52dfd744
Destinat0r <ashavrn@protonmail.com>
2020-01-07 21:03:53 +0200

    added repository, findForToday()

--- src/main/java/all/that/matters/services/StatisticService.java
+++ src/main/java/all/that/matters/services/StatisticService.java
@@ -1,5 +1,25 @@
 package all.that.matters.services;
 
+import all.that.matters.dao.StatisticRepository;
+import all.that.matters.domain.Statistic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
 public class StatisticService {
 
+    private StatisticRepository statisticRepository;
+
+    @Autowired
+    public StatisticService(StatisticRepository statisticRepository) {
+        this.statisticRepository = statisticRepository;
+    }
+
+    public List<Statistic> findForToday() {
+        LocalDate today = LocalDate.now();
+        return statisticRepository.findAllByDateTime_DayOfYear(today.getDayOfYear());
+    }
 }
