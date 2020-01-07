fae62ed22cdc3336b48c398c08d5b1968016bbfe
Destinat0r <ashavrn@protonmail.com>
2020-01-07 20:01:13 +0200

    changed method to get findAllByDateTime_DayOfYear()

--- src/main/java/all/that/matters/dao/StatisticRepository.java
+++ src/main/java/all/that/matters/dao/StatisticRepository.java
@@ -4,10 +4,9 @@ import all.that.matters.domain.Statistic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface StatisticRepository extends JpaRepository<Statistic, Long> {
-    List<Statistic> findAllByDateTime(LocalDateTime today);
+    List<Statistic> findAllByDateTime_DayOfYear(int dayOfYear);
 }
