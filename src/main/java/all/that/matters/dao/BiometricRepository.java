116107c211538940601e0a6516777c31f6673348
Destinat0r <ashavrn@protonmail.com>
2020-01-08 12:19:10 +0200

    added @Repository

--- src/main/java/all/that/matters/dao/BiometricRepository.java
+++ src/main/java/all/that/matters/dao/BiometricRepository.java
@@ -2,7 +2,9 @@ package all.that.matters.dao;
 
 import all.that.matters.domain.Statistic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BiometricRepository extends JpaRepository<Statistic, Long> {
-    
+
 }
