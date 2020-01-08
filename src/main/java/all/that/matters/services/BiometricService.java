caa82877dd66d0c9f5372949928da68c2c170998
Destinat0r <ashavrn@protonmail.com>
2020-01-08 15:19:02 +0200

    added BiometricService

--- src/main/java/all/that/matters/services/BiometricService.java
+++ src/main/java/all/that/matters/services/BiometricService.java
@@ -1,5 +1,20 @@
 package all.that.matters.services;
 
-public class BiometricService {
+import all.that.matters.dao.BiometricRepository;
+import all.that.matters.domain.Biometrics;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+public class BiometricService {
+    private BiometricRepository biometricRepository;
+
+    @Autowired
+    public BiometricService(BiometricRepository biometricRepository) {
+        this.biometricRepository = biometricRepository;
+    }
+
+    public void create(Biometrics biometrics) {
+        biometricRepository.save(biometrics);
+    }
 }
