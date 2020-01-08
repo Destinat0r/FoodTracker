daed36cb686033fbe42ec917fabcba8905b47ee2
Destinat0r <ashavrn@protonmail.com>
2020-01-08 17:43:04 +0200

    commented out default link

--- src/main/java/all/that/matters/config/WebSecurityConfig.java
+++ src/main/java/all/that/matters/config/WebSecurityConfig.java
@@ -41,7 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .formLogin()
                     .loginPage("/login")
-                    .defaultSuccessUrl("/main")
+//                    .defaultSuccessUrl("/main")
                     .permitAll()
                 .and()
                     .logout()
