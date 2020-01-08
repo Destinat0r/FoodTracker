c7c918c3660bfceab40f05cde54ae3d26c7d2e8e
Destinat0r <ashavrn@protonmail.com>
2020-01-08 21:01:25 +0200

    removed defauld consumedToday value

--- src/main/java/all/that/matters/domain/Biometrics.java
+++ src/main/java/all/that/matters/domain/Biometrics.java
@@ -43,7 +43,7 @@ public class Biometrics {
     private Double dailyNorm;
 
     @Column(name = "consumed_today")
-    private Double consumedToday = 0.0;
+    private Double consumedToday;
 
     /**
      * Total energy expenditure calculation using Harris–Benedict equation
