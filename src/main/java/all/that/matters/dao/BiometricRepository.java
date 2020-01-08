310daf08b68cebbe103005ff462639a0aee808af
Destinat0r <ashavrn@protonmail.com>
2020-01-08 15:18:20 +0200

    fixed type in definition

--- src/main/java/all/that/matters/dao/BiometricRepository.java
+++ src/main/java/all/that/matters/dao/BiometricRepository.java
@@ -1,10 +1,11 @@
 package all.that.matters.dao;
 
+import all.that.matters.domain.Biometrics;
 import all.that.matters.domain.Statistic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BiometricRepository extends JpaRepository<Statistic, Long> {
+public interface BiometricRepository extends JpaRepository<Biometrics, Long> {
 
 }
