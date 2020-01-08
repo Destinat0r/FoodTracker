1992dd5e5d9305e19ee8fcf3ad207b90897175ad
Destinat0r <ashavrn@protonmail.com>
2020-01-08 11:28:47 +0200

    fixed query to have correct column timestamp

--- src/main/java/all/that/matters/dao/StatisticRepository.java
+++ src/main/java/all/that/matters/dao/StatisticRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface StatisticRepository extends JpaRepository<Statistic, Long> {
 
-    @Query(value = "SELECT id, user_id, food_id, amount, action, date_trunc('day', timestamp) FROM statistics WHERE user_id = ?1"
+    @Query(value = "SELECT id, user_id, food_id, amount, action, timestamp FROM statistics WHERE user_id = ?1"
                            + " AND date_trunc('day', timestamp) >= current_date",
     nativeQuery = true)
     List<Statistic> findAllFromTodayByUserId(Long userId);
