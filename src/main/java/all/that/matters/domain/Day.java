5d8df50a87b5c7fe831ac7936b31febb5a7bc623
Destinat0r <ashavrn@protonmail.com>
2020-01-10 10:31:26 +0200

    added Day

--- src/main/java/all/that/matters/domain/Day.java
+++ src/main/java/all/that/matters/domain/Day.java
@@ -1,5 +1,28 @@
 package all.that.matters.domain;
 
-public class Day {
+import lombok.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import java.time.LocalDate;
+import java.util.List;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Entity
+public class Day {
+    private LocalDate date;
+    private User user;
+    private List<Food> consumed;
+
+    @Column(name = "consumed_today")
+    private Double consumedToday = 0.0;
+
+    private boolean isNormExceeded;
 }
+
