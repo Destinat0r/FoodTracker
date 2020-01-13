d2774c93d168bbf399a681fb90a959ce895cf199
Destinat0r <ashavrn@protonmail.com>
2020-01-14 01:42:25 +0200

    added defaultSuccessUrl(/redirect)

--- src/main/java/all/that/matters/config/WebSecurityConfig.java
+++ src/main/java/all/that/matters/config/WebSecurityConfig.java
@@ -40,7 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .formLogin()
                     .loginPage("/login")
-//                    .defaultSuccessUrl("/main")
+                    .defaultSuccessUrl("/redirect")
                     .permitAll()
                 .and()
                     .logout()
