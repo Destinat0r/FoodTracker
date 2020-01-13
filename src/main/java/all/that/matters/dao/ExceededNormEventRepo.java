da7ca80e02f37e2eaf0450313fe0f593749f0a84
Destinat0r <ashavrn@protonmail.com>
2020-01-13 14:57:14 +0200

    added updateExcessiveCaloriesByDateAndUserId()

--- src/main/java/all/that/matters/dao/ExceededNormEventRepo.java
+++ src/main/java/all/that/matters/dao/ExceededNormEventRepo.java
@@ -2,9 +2,19 @@ package all.that.matters.dao;
 
 import all.that.matters.domain.ExceededNormEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @Repository
 public interface ExceededNormEventRepo extends JpaRepository<ExceededNormEvent, Long> {
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE exceed_events SET excessive_calories = ?1 WHERE date = ?2 AND user_id = ?3", nativeQuery = true)
+    void updateExcessiveCaloriesByDateAndUserId(Double calories, LocalDate date, Long userId);
 
 }
