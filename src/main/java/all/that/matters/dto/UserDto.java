9d147fe237bd685cb64b527bb525191296f70be0
Destinat0r <ashavrn@protonmail.com>
2020-01-18 22:37:16 +0200

    added constraints to UserDTO

--- src/main/java/all/that/matters/dto/UserDto.java
+++ src/main/java/all/that/matters/dto/UserDto.java
@@ -5,6 +5,7 @@ import all.that.matters.model.Role;
 import all.that.matters.model.Sex;
 import lombok.*;
 
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 
 @Getter
@@ -13,17 +14,35 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @Builder
 
+
 public class UserDTO {
+
+    @NotBlank(message = "Username cannot be empty")
     private String username;
+
+    @NotBlank(message = "Password cannot be empty")
+    private String password;
+
+    @NotBlank(message = "Name cannot be empty")
     private String fullName;
+
+    @NotBlank(message = "Name cannot be empty")
     private String nationalName;
+
+    @Email(message = "Email is not correct")
     private String email;
     private Role role;
+
+    @Min(value = 1, message = "Incorrect weight")
     private BigDecimal age;
     private Sex sex;
+
+    @Min(value = 1, message = "Incorrect weight")
     private BigDecimal weight;
+
+    @Min(value = 1, message = "Incorrect height")
     private BigDecimal height;
     private Lifestyle lifestyle;
     private BigDecimal dailyNorm;
-    private String password;
+
 }
