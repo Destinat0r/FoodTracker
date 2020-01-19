abc51ecb544deca15bafb66337b4f53039767a41
Destinat0r <ashavrn@protonmail.com>
2020-01-19 21:19:19 +0200

    changed @NotEmpty to @NotNull for BigDecimals

--- src/main/java/all/that/matters/dto/UserDTO.java
+++ src/main/java/all/that/matters/dto/UserDTO.java
@@ -18,43 +18,43 @@ import java.math.BigDecimal;
 
 public class UserDTO {
 
-    @NotBlank(message = "Please enter username")
-    @Size(min = 2, max = 32, message = "username must be between 2 and 32 characters long")
+    @NotBlank(message = "{userDTO.constraints.username.not_blank}")
+    @Size(min = 2, max = 32, message = "{userDTO.constraints.username.size}")
     private String username;
 
-    @NotBlank(message = "Please enter password")
-    @Size(min = 4, max = 32, message = "Password must be between 4 and 32 characters long")
+    @NotBlank(message = "{userDTO.constraints.password.not_blank}")
+    @Size(min = 4, max = 32, message = "{userDTO.constraints.password.size}")
     private String password;
 
-    @NotBlank(message = "Please enter full name")
-    @Size(min = 2, max = 32, message = "Full name must be between 2 and 32 characters long")
+    @NotBlank(message = "{userDTO.constraints.full_name.not_blank}")
+    @Size(min = 2, max = 32, message = "{userDTO.constraints.full_name.size}")
     private String fullName;
 
-    @NotBlank(message = "Please enter national name")
-    @Size(min = 2, max = 32, message = "National name must be between 2 and 32 characters long")
+    @NotBlank(message = "{userDTO.constraints.national_name.not_blank}")
+    @Size(min = 2, max = 32, message = "{userDTO.constraints.national_name.size}")
     private String nationalName;
 
-    @NotBlank(message = "Please enter email")
-    @Email(message = "Email is not correct")
+    @NotBlank(message = "{userDTO.constraints.email.not_blank}")
+    @Email(message = "{userDTO.constraints.email.size}")
     private String email;
 
     private Role role;
 
-    @NotEmpty(message = "Please enter age")
-    @Min(value = 18, message = "Sorry, you should be at least 18 years old to use our service")
-    @Max(value = 120, message = "Sorry, age upper than 200 is not supported.")
+    @NotNull(message = "{userDTO.constraints.age.not_blank}")
+    @Min(value = 18, message = "{userDTO.constraints.age.min}")
+    @Max(value = 200, message = "{userDTO.constraints.age.max}")
     private BigDecimal age;
 
     private Sex sex;
 
-    @NotEmpty(message = "Please enter weight")
-    @Min(value = 30, message = "Weight should be at least 30")
-    @Max(value = 200, message = "Sorry, weight bigger than 200 is not supported.")
+    @NotNull(message = "{userDTO.constraints.weight.not_blank}")
+    @Min(value = 1, message = "{userDTO.constraints.weight.min}")
+    @Max(value = 500, message = "{userDTO.constraints.weight.max}")
     private BigDecimal weight;
 
-    @NotEmpty(message = "Please enter weight")
-    @Min(value = 1, message = "Height should be at bigger than 0")
-    @Max(value = 300, message = "Sorry, height upper than 300 is no supported")
+    @NotNull(message = "{userDTO.constraints.height.not_blank}")
+    @Min(value = 1, message = "{userDTO.constraints.height.min}")
+    @Max(value = 300, message = "{userDTO.constraints.height.max}")
     private BigDecimal height;
     private Lifestyle lifestyle;
     private BigDecimal dailyNorm;
