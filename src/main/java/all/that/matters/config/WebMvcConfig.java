03e1de4ec5a557d6b11fcd4cb87ba6effdb732d6
Destinat0r <ashavrn@protonmail.com>
2020-01-21 13:02:37 +0200

    removed resourceHandler for webjars

--- src/main/java/all/that/matters/config/WebMvcConfig.java
+++ src/main/java/all/that/matters/config/WebMvcConfig.java
@@ -21,9 +21,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("/webjars/");
-
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
     }
