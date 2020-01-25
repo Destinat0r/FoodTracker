20289178b79901f9e0f39ecfbd419c55e4134bb3
Destinat0r <ashavrn@protonmail.com>
2020-01-25 11:52:08 +0200

    added logs to create()

--- src/main/java/org/training/food_tracker/controller/RegistrationController.java
+++ src/main/java/org/training/food_tracker/controller/RegistrationController.java
@@ -1,5 +1,7 @@
 package org.training.food_tracker.controller;
 
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.training.food_tracker.dto.UserDTO;
 import org.training.food_tracker.model.Biometrics;
 import org.training.food_tracker.model.User;
@@ -16,6 +18,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
+
+@Slf4j
+@Log4j2
 
 @Controller
 public class RegistrationController {
@@ -57,12 +62,16 @@ public class RegistrationController {
         biometrics.setDailyNorm();
 
         try {
+            log.debug("Creating and setting biometrics to user");
             biometricService.create(biometrics);
             user.setBiometrics(biometrics);
             biometrics.setOwner(user);
+
+            log.debug("Creating user");
             userService.create(user);
         } catch (UserExistsException e) {
             model.addAttribute("usernameError", "Such username or email already exists!");
+            log.error("Failed to create the user", e);
             return "registration";
         }
 
