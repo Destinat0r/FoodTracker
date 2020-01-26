25885c03b5215d5e33231a5f8a9eb4ff7d33f158
Destinat0r <ashavrn@protonmail.com>
2020-01-26 08:52:55 +0200

    added calculateDailyNorm()

--- src/main/java/org/training/food_tracker/services/BiometricService.java
+++ src/main/java/org/training/food_tracker/services/BiometricService.java
@@ -1,10 +1,14 @@
 package org.training.food_tracker.services;
 
 import org.training.food_tracker.dto.UserDTO;
+import org.training.food_tracker.model.Lifestyle;
+import org.training.food_tracker.model.Sex;
 import org.training.food_tracker.repo.BiometricRepository;
 import org.training.food_tracker.model.Biometrics;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class BiometricService {
@@ -29,7 +33,22 @@ public class BiometricService {
                 userDTO.getWeight(),
                 userDTO.getHeight(),
                 userDTO.getLifestyle().toString(),
-                userDTOtoBiometrics(userDTO).calculateDailyNorm());
+                calculateDailyNorm(userDTO));
+    }
+
+    private BigDecimal calculateDailyNorm(UserDTO userDTO) {
+        if (userDTO.getSex() == Sex.MALE) {
+            return (new BigDecimal(66.5)
+                            .add(new BigDecimal(13.75).multiply(userDTO.getWeight()))
+                            .add(new BigDecimal(5.003).multiply(userDTO.getHeight()))
+                            .subtract(new BigDecimal(6.755).multiply(userDTO.getAge())))
+                           .multiply(userDTO.getLifestyle().getCoefficient());
+        }
+        return (new BigDecimal(655.1)
+                        .add(new BigDecimal(9.563).multiply(userDTO.getWeight()))
+                        .add(new BigDecimal(1.850).multiply(userDTO.getHeight()))
+                        .subtract(new BigDecimal(4.676).multiply(userDTO.getAge())))
+                       .multiply(userDTO.getLifestyle().getCoefficient());
     }
 
     public Biometrics userDTOtoBiometrics(UserDTO userDTO) {
