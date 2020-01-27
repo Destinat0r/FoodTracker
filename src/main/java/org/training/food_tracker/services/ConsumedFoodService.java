c047495799f118adca5eeadc5daa5a76236b1463
Destinat0r <ashavrn@protonmail.com>
2020-01-27 15:24:02 +0200

    adding calories for consumedFood and day, added logs

--- src/main/java/org/training/food_tracker/services/ConsumedFoodService.java
+++ src/main/java/org/training/food_tracker/services/ConsumedFoodService.java
@@ -1,5 +1,7 @@
 package org.training.food_tracker.services;
 
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.training.food_tracker.dto.FoodDTO;
@@ -10,6 +12,9 @@ import org.training.food_tracker.repo.ConsumedFoodRepo;
 import org.training.food_tracker.utils.ContextUtils;
 
 import java.time.LocalTime;
+
+@Slf4j
+@Log4j2
 
 @Service
 public class ConsumedFoodService {
@@ -25,14 +30,22 @@ public class ConsumedFoodService {
     }
 
     public void registerConsumption(FoodDTO foodDTO) {
-        User user = ContextUtils.getPrincipal();
-        Day day = dayService.getCurrentDayOfUser(user);
+        log.debug("method: registerConsumption");
 
+        User user = ContextUtils.getPrincipal();
+
+        Day day = dayService.getCurrentDayOfUser(user);
+        log.debug("current day of the user: {}", day);
+
+        log.debug("adding consumed calories to the current day:");
+        day.addCalories(foodDTO.getTotalCalories().multiply(foodDTO.getAmount()));
+
+        log.debug("saving consumed food from foodDTO: {}", foodDTO);
         consumedFoodRepo.save(
                 ConsumedFood.builder()
                         .food(foodDTO.getName())
                         .amount(foodDTO.getAmount())
-                        .totalCalories(foodDTO.getTotalCalories())
+                        .totalCalories(foodDTO.getTotalCalories().multiply(foodDTO.getAmount()))
                         .day(day)
                         .time(LocalTime.now())
                         .build()
